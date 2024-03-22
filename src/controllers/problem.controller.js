b98a75f405621ff92236d8fa151a030ead514f9f
geektousif <syedmdtousif.2002@gmail.com>
2024-03-22 23:41:50 +0530

    Added Error Handler and Error Classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,7 +1,15 @@
+const NotImplemented = require("../errors/notImplemented.error");
+
 function pingProblem(req, res) {
   res.json({ message: "problem controller check ok" });
 }
-function addProblem(req, res) {}
+function addProblem(req, res) {
+  try {
+    throw new NotImplemented("addProblem");
+  } catch (error) {
+    next(error);
+  }
+}
 function getProblem(req, res) {}
 function getProblems(req, res) {}
 function deleteProblem(req, res) {}
